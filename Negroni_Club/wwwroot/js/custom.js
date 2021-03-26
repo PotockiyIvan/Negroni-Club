5ec54e74c789c114d6106541fb259b6a9a7b9c2d
PotockiyIvan <67115644+PotockiyIvan@users.noreply.github.com>
2021-03-26 04:50:14 +0300

    Установлено видео на баннер,блок специальных предложений в процеесе изменения на блок с событиями.

--- Negroni_Club/wwwroot/js/custom.js
+++ Negroni_Club/wwwroot/js/custom.js
@@ -60,11 +60,12 @@
 
     $(document).ready(function () {
         $("#owl-demo").owlCarousel({
-            autoPlay: 3000, //Set AutoPlay to 3 seconds
-            items: 3,
-            itemsDesktop: [1199, 3],
-            itemsDesktopSmall: [979, 2]
-
+            autoPlay: 5000, //Set AutoPlay to 3 seconds
+            items: 1,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [979, 1],
+            singleItem: true,
+            dots: false,
         });
     });
 
