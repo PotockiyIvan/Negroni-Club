3ac9393b73bc3a852e5fa4ed35b56dd0248fa04e
PotockiyIvan <67115644+PotockiyIvan@users.noreply.github.com>
2021-03-30 02:35:26 +0300

    Работа над блоком с событиями.

--- Negroni_Club/wwwroot/js/custom.js
+++ Negroni_Club/wwwroot/js/custom.js
@@ -60,12 +60,14 @@
 
     $(document).ready(function () {
         $("#owl-demo").owlCarousel({
-            autoPlay: 5000, //Set AutoPlay to 3 seconds
+            /*autoPlay: 5000,*/ //Set AutoPlay to 3 seconds
             items: 1,
-            itemsDesktop: [1199, 1],
-            itemsDesktopSmall: [979, 1],
+            animateIn: 'fadeIn',
+            animateOut: 'bounceOutUp',
             singleItem: true,
             dots: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [979, 1]
         });
     });
 
